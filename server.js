469b3361cab69ae282732e6f4f075b3402e13030
Riddhi Gupta <103330938+riddzz04@users.noreply.github.com>
2024-01-26 00:38:37 +0530

    Final code
    My project

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//env config
+dotenv.config();
+
+//router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogroutes");
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  // Optionally, terminate the process if needed
+  process.exit(1);
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error(`Uncaught Exception: ${err.message}`);
+  // Optionally, terminate the process if needed
+  process.exit(1);
+});
+
+// Port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan.white
+  );
+});
